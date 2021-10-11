c247e4879a627c1a1ecf4679242ce998a190c631
Hliang7555 <91446562+Hliang7555@users.noreply.github.com>
2021-10-10 21:36:11 -0700

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,33 @@
+
+function bigger() {
+	hello();
+	var words = document.getElementById("words");
+	words.style.fontSize="24pt";
+}
+
+function hello() {
+alert("Hello, world!");
+}
+
+function fancy() {
+	var check = document.getElementById("fancy"); 
+	var words = document.getElementById("words");
+	if(check.checked) {
+		hello();
+		words.style.fontWeight = "bold";
+		words.style.color = "blue";
+		words.style.textDecoration = "underline blink";
+	} else {
+		words.style.fontWeight = "normal";
+		words.style.color = "black";
+		words.style.textDecoration = "none";
+	}
+} 
+
+
+function moo() {
+	var words = document.getElementById("words");
+	var per = words.value.split(".");
+	words.value = per.join("-Moo.");
+	words.style="text-transform:uppercase";
+}
